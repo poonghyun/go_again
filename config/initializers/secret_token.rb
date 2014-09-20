1614eaa6271768f6b3f10671d008b3f3494ff3bb
poonghyun <michael.ph.lee.12@gmail.com>
2014-09-20 15:18:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoAgain::Application.config.secret_key_base = '478e73437f14e24817da95b8ca6282cdae163d050465479fa6e98909f0ce0526cd11025e6abfc94b1e587d5a9bef2f3047ee1e8f50e96a248ace53676c7bb3c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoAgain::Application.config.session_store :cookie_store, key: '_go_again_session'
