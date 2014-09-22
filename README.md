adcdf1f73beab52d63dc5e2dfe50ccd2a13108dd
poonghyun <michael.ph.lee.12@gmail.com>
2014-09-21 18:07:01 -0700

    minor navbar modifications

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ todo:
 -profile images
 -logo
 -about me backbone form
+-center search bar
+-buttons for categories on root static
+-add more categories
+-ability to add photos to businesses/reviews
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,4 +3,8 @@ class Business < ActiveRecord::Base
 	validates :b_type, inclusion: { in: ["Bars", "Restaurants", "Cafes"], message: "not a valid type" }
 	
 	has_many :reviews
+
+	def self.find_by_category(category)
+		self.where(b_type: category)
+	end
 end
