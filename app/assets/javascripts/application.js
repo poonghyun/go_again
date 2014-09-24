08376c3c8323b0d86309eea6185725c9988a4d6a
poonghyun <michael.ph.lee.12@gmail.com>
2014-09-24 11:45:48 -0700

    google maps, add composite view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require bootstrap
 //= require go_again
 //= require_tree ../templates
+//= require_tree ./utils
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
